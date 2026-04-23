4cc683515434a44c3437bbe83f9e4d0e7e443712
Anindya Sarkar <sanindya710@gmail.com>
2026-04-23 15:46:56 +0530

    Update App and page components

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import './App.css';
 import { useState,useEffect, use } from 'react';
 import Page from './components/page';
 
-async function api(index){
-    let req=await fetch(`https://picsum.photos/v2/list?page=${index}&limit=10`);
-    let data=await req.json();
-    return data;
-}
-
 
 export default function App(){
+  
+  async function api(index){
+      let req=await fetch(`https://picsum.photos/v2/list?page=${index}&limit=10`);
+      let data=await req.json();
+      return data;
+  }
+  
   const [index,setIndex]=useState(1);
+  let data=api(index);
 
-  function Load(){
+  useEffect(()=>{
+    data=api(index);
+  },[index])
+
+  function Prev(){
     if (index>1){
       setIndex(index-1)
     }
@@ -21,11 +27,12 @@ export default function App(){
   function Next(){
     setIndex(index+1)
   }
+
   return (
     <div>
-      {/* <Page/> */}
+      <Page info={data}/>
       <div style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-      <button onClick={Load}>Prev</button>
+      <button onClick={Prev}>Prev</button>
       <h3 style={{display:'inline',color:'white',marginLeft:'15px',marginRight:'15px'}}>Page number: {index}</h3>
       <button onClick={Next}>Next</button>
       </div>
